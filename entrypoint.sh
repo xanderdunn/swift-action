0cbf680e918312c62d1087b3075355cd6c270f8f
Xander Dunn <xander@xander.ai>
2020-09-21 11:00:07 -0700

    Add flags to be able to find HDF5

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-swift build -v
-swift test -v
+swift build -v -Xlinker --export-dynamic -Xswiftc -g -Xlinker -L/usr/lib/x86_64-linux-gnu/hdf5/serial -Xcc -I/usr/include/hdf5/serial/
+swift test -v -Xlinker --export-dynamic -Xswiftc -g -Xlinker -L/usr/lib/x86_64-linux-gnu/hdf5/serial -Xcc -I/usr/include/hdf5/serial/
